9a09ed99daf107318eb2012d5066c9386b785df5
s.vigneswaran <s.vigneswaran@VDESNTCS0056.rci.rogers.ca>
2016-10-17 01:37:38 -0400

    Initial commit

---
+++ SampleGit/src/com/vicky/application/commerce/formhandlers/CartUtil.java
@@ -0,0 +1,18 @@
+package com.vicky.application.commerce.formhandlers;
+
+public class CartUtil {
+private String cartId;
+private String description;
+public String getCartId() {
+	return cartId;
+}
+public void setCartId(String cartId) {
+	this.cartId = cartId;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+}
