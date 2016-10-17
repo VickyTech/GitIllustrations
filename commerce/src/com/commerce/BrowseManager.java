6aa48b7f397e138637e3f9e6a8cf535c5824d2c9
vigneswaran <vicky_sitaraman@yahoo.com>
2016-10-17 22:13:00 +0530

    commit inital commerce project

---
+++ commerce/src/com/commerce/BrowseManager.java
@@ -0,0 +1,18 @@
+package com.commerce;
+
+public class BrowseManager {
+private String message;
+private boolean enabled;
+public String getMessage() {
+	return message;
+}
+public void setMessage(String message) {
+	this.message = message;
+}
+public boolean isEnabled() {
+	return enabled;
+}
+public void setEnabled(boolean enabled) {
+	this.enabled = enabled;
+}
+}
