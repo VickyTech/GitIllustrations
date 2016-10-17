fec40fe353c17c915a3ae3be71832cbbc4ef99bf
s.vigneswaran <s.vigneswaran@VDESNTCS0056.rci.rogers.ca>
2016-10-17 03:03:02 -0400

    changed sum

--- SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
+++ SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
@@ -3,15 +3,13 @@ package com.vicky.application.commerce.formhandlers;
 public class CartModifierFormHandler {
 public static void main(String args){
 
-<<<<<<< HEAD
+
 	int i=0;
 	System.out.println("print i"+i);
 
-=======
-		System.out.println("deleted this statement");
 	int sum=0;
 	System.out.println("sum"+sum);
 
->>>>>>> branch 'dev-branch' of ssh://git@github.com/VickyTech/GitIllustrations.git
+
 }
 }
