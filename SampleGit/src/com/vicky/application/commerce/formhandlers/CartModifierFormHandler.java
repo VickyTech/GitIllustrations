1e5d15a5dbdc69cd1678893e5761868142b95668
VickyTech <vicky_sitaraman@yahoo.com>
2016-10-17 02:36:16 -0400

    updated the print statement

--- SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
+++ SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
@@ -2,6 +2,6 @@ package com.vicky.application.commerce.formhandlers;
 
 public class CartModifierFormHandler {
 public static void main(String args){
-		System.out.println("add item to order");
+		System.out.println("delet this");
 }
 }
