19e889a308e72500d49104cc742c8597d5e3b610
VickyTech <vicky_sitaraman@yahoo.com>
2016-10-17 23:16:36 +0530

    removed the print line

--- SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
+++ SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
@@ -2,6 +2,8 @@ package com.vicky.application.commerce.formhandlers;
 
 public class CartModifierFormHandler {
 public static void main(String args){
-	System.out.println("r1116 fix");
+	int i=0;
+	//removed the print line
+
 }
 }
