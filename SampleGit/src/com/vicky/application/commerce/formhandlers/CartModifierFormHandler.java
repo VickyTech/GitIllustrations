8851d7df061958c6401d2f4c5d0e30e0fb757f7e
vigneswaran <vicky_sitaraman@yahoo.com>
2016-10-17 23:25:04 +0530

    another print statement changes

--- SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
+++ SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
@@ -4,7 +4,7 @@ public class CartModifierFormHandler {
 public static void main(String args){
 	int i=0;
 	//removed the print line
-	System.out.println("implementation");
+	System.out.println("implementation1");
 
 }
 }
