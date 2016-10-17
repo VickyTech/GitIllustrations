03389b9c20cddd9bab9744f5aa529d156c47c6e1
vigneswaran <vicky_sitaraman@yahoo.com>
2016-10-17 23:12:03 +0530

    r1116 fix

--- SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
+++ SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
@@ -2,6 +2,6 @@ package com.vicky.application.commerce.formhandlers;
 
 public class CartModifierFormHandler {
 public static void main(String args){
-	System.out.println("Cart modifier handler");
+	System.out.println("r1116 fix");
 }
 }
