62ac2712c905140713be18fbe3e514e805c85eba
s.vigneswaran <s.vigneswaran@VDESNTCS0056.rci.rogers.ca>
2016-10-17 02:25:11 -0400

    Deleted print message

--- SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
+++ SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
@@ -2,7 +2,6 @@ package com.vicky.application.commerce.formhandlers;
 
 public class CartModifierFormHandler {
 public static void main(String args){
-	System.out.println("development-fix modifier handler");
-	System.out.println("add item to order");
+		System.out.println("add item to order");
 }
 }
