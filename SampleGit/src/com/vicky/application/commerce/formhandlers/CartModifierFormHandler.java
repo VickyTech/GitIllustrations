4d4ee9f74d878e6b55e572ea833591d32b760f76
s.vigneswaran <s.vigneswaran@VDESNTCS0056.rci.rogers.ca>
2016-10-17 02:46:50 -0400

    printed i

--- SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
+++ SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
@@ -3,7 +3,8 @@ package com.vicky.application.commerce.formhandlers;
 public class CartModifierFormHandler {
 public static void main(String args){
 
-		System.out.println("deleted this statement");
+	int i=0;
+	System.out.println("print i"+i);
 
 }
 }
