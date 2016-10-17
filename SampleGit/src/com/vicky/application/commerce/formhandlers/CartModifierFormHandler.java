856ed550ccdc5a1d0af131a62c279b9bc113741e
VickyTech <vicky_sitaraman@yahoo.com>
2016-10-17 02:15:35 -0400

    Update CartModifierFormHandler.java

--- SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
+++ SampleGit/src/com/vicky/application/commerce/formhandlers/CartModifierFormHandler.java
@@ -2,7 +2,7 @@ package com.vicky.application.commerce.formhandlers;
 
 public class CartModifierFormHandler {
 public static void main(String args){
-	System.out.println("development modifier handler");
+	System.out.println("development-fix modifier handler");
 	System.out.println("add item to order");
 }
 }
